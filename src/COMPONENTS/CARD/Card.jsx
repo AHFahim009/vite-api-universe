e9ffd754871761998bde6d6373288786216358ec
Ahfahim009 <ahfba009@gmail.com>
2023-03-26 22:46:34 +0600

    second commit

--- src/COMPONENTS/CARD/Card.jsx
+++ src/COMPONENTS/CARD/Card.jsx
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Button from "../BUTTON/Button";
+import Modal from "../MODAL/Modal";
 import SingleData from "../SINGLEDATA/SingleData";
 
 const Card = () => {
   const [data, setData] = useState([]);
   const [showAll, setShowAll] = useState(false);
+  const [uniqueID, setUniqueId] = useState(null);
+  const [singleData, setSingleData] = useState({});
+  //   console.log(uniqueID);
   const btnSeeMore = () => {
     setShowAll(true);
   };
+  useEffect(() => {
+    fetch(`https://openapi.programming-hero.com/api/ai/tool/${uniqueID}`)
+      .then((res) => res.json())
+      .then((data) => setSingleData(data.data));
+  }, [uniqueID]);
+  console.log(singleData);
+
   useEffect(() => {
     const loadData = async () => {
       const response = await fetch(
@@ -23,12 +34,19 @@ const Card = () => {
     <>
       <div className=" lg:px-11 gap-3 mt-4 grid grid-cols-1 md: grid-cols-2 lg:grid-cols-3">
         {data.slice(0, showAll ? 12 : 6).map((singleData) => (
-          <SingleData key={singleData.id} singleData={singleData}></SingleData>
+          <SingleData
+            key={singleData.id}
+            singleData={singleData}
+            setUniqueId={setUniqueId}
+          ></SingleData>
         ))}
       </div>
-      <span onClick={btnSeeMore}>
-        <Button>See More</Button>
-      </span>
+      {!showAll && (
+        <span onClick={btnSeeMore}>
+          <Button>See More</Button>
+        </span>
+      )}
+      <Modal></Modal>
     </>
   );
 };
